a02e97469dd0bd4da9f82172a5036e3d9fed66eb
Robert Nowakowski <rnowakowski.prv@gmail.com>
2018-05-02 21:35:44 +0200

    Added default contacts array in state and rendering contacts list from state

--- src/App.js
+++ src/App.js
@@ -6,7 +6,23 @@ class App extends Component {
     contactName: '',
     contactPhone: '',
     contactEmail: '',
-    contactCategories: ''
+    contactCategories: '',
+    contacts: [
+      {
+        id: 1,
+        contactName: 'Abc Def',
+        contactPhone: '123-456-789',
+        contactEmail: 'abc@abc.com',
+        contactCategories: 'abc, def'
+      },
+      {
+        id: 2,
+        contactName: 'Ghi Jkl',
+        contactPhone: '987-654-321',
+        contactEmail: 'ghi@ghi.com',
+        contactCategories: 'ghi , jkl jjj,ooo'
+      }
+    ]
   };
 
   handleSubmit = event => {
@@ -58,6 +74,22 @@ class App extends Component {
             <p>700-400-300, jconnor@skynet.com</p>
             <p>[family], [starred]</p>
           </li>
+          {
+            this.state.contacts.map(
+              contact =>
+                <li key={contact.id}>
+                  <p><strong>{contact.contactName}</strong></p>
+                  <p>{contact.contactPhone}, {contact.contactEmail}</p>
+                  <p>{contact.contactCategories.split(',').map(
+                    (category, index) => {
+                      const categoryDisplay = `[${category.trim()}]`;
+                      const categoriesLastIndex = contact.contactCategories.split(',').length - 1;
+                      return index === categoriesLastIndex ? categoryDisplay : categoryDisplay + `, `;
+                    }
+                  )}</p>
+                </li>
+            )
+          }
         </ul>
       </div>
     );
