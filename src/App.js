51dcd3fd4318470a93d2aacbf08351b1aa5be7dc
Robert Nowakowski <rnowakowski.prv@gmail.com>
2018-05-02 20:02:17 +0200

    Added form fields and first placeholder contact

--- src/App.js
+++ src/App.js
@@ -4,7 +4,22 @@ class App extends Component {
   render() {
     return (
       <div>
+        <h2>Contacts</h2>
+        <form>
+          <input placeholder={'First and last name'}/>
+          <input placeholder={'Phone number'}/>
+          <input placeholder={'Email address'}/>
+          <input placeholder={'Categories'}/>
+          <button>Add</button>
+        </form>
 
+        <ul>
+          <li>
+            <p><strong>John Connor</strong></p>
+            <p>700-400-300, jconnor@skynet.com</p>
+            <p>[family], [starred]</p>
+          </li>
+        </ul>
       </div>
     );
   }
