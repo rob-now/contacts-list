6ea4c866240b1f924555cb8c7c94f65d2c5bcb78
Robert Nowakowski <rnowakowski.prv@gmail.com>
2018-05-03 22:42:21 +0200

    WIP: edit contact

--- src/ContactList.js
+++ src/ContactList.js
@@ -1,25 +1,45 @@
-import React, {Component} from 'react'
+import React, {Component, Fragment} from 'react'
 
 class ContactList extends Component {
+
+  state = {
+    editContactId: null
+  };
+
+  enterEditMode = contactId =>
+    this.setState({
+      editContactId: contactId
+    });
+
   render() {
-    return(
+    console.log(this.state.editContactId);
+    return (
       <ul>
         {
           this.props.contacts.map(
             contact =>
               <li key={contact.id}>
-                <p><strong>{contact.contactName}</strong></p>
-                <p>{contact.contactPhone}, {contact.contactEmail}</p>
-                <p>{contact.contactCategories.split(',').map(
-                  (category, index) => {
-                    const categoryDisplay = `[${category.trim()}]`;
-                    const categoriesLastIndex = contact.contactCategories.split(',').length - 1;
-                    return index === categoriesLastIndex ? categoryDisplay : categoryDisplay + `, `;
-                  }
-                )}</p>
-                <button onClick={() => this.props.removeContact(contact.id)}>
-                  Delete
-                </button>
+                {
+                  this.state.editContactId === contact.id
+                    ? <p>in edit</p>
+                    : <Fragment>
+                      <p><strong>{contact.contactName}</strong></p>
+                      <p>{contact.contactPhone}, {contact.contactEmail}</p>
+                      <p>{contact.contactCategories.split(',').map(
+                        (category, index) => {
+                          const categoryDisplay = `[${category.trim()}]`;
+                          const categoriesLastIndex = contact.contactCategories.split(',').length - 1;
+                          return index === categoriesLastIndex ? categoryDisplay : categoryDisplay + `, `;
+                        }
+                      )}</p>
+                      <button onClick={() => this.enterEditMode(contact.id)}>
+                        Edit
+                      </button>
+                      <button onClick={() => this.props.removeContact(contact.id)}>
+                        Delete
+                      </button>
+                    </Fragment>
+                }
               </li>
           )
         }
